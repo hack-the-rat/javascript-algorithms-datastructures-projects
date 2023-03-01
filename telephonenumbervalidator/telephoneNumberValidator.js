5f2b3e8f2b00e0002d41fd73c45b515d8d0e0691
Robert Taylor <55753991+hack-the-rat@users.noreply.github.com>
2023-03-01 15:57:07 -0500

    Create telephoneNumberValidator.js

---
+++ telephonenumbervalidator/telephoneNumberValidator.js
@@ -0,0 +1,17 @@
+function telephoneCheck(str) {
+  const validPatterns = [
+    /^\d{3}-\d{3}-\d{4}$/,
+    /^\(\d{3}\)\d{3}-\d{4}$/,
+    /^\(\d{3}\) \d{3}-\d{4}$/,
+    /^\d{3} \d{3} \d{4}$/,
+    /^\d{10}$/,
+    /^1 \d{3} \d{3} \d{4}$/,
+    /^1 \d{3}-\d{3}-\d{4}$/,
+    /^1 \(\d{3}\) \d{3}-\d{4}$/,
+    /^1\(\d{3}\)\d{3}-\d{4}$/
+  ];
+
+  return validPatterns.some((pattern) => pattern.test(str));
+}
+
+telephoneCheck("555-555-5555");
